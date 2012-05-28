d494f65c0be401dec8f65340599baf6f92ab2042
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-05-28 15:05:39 +0300

    using bootstrap

--- app.js
+++ app.js
@@ -5,9 +5,19 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , http = require('http');
+  , http = require('http')
+  , stylus = require('stylus')
+  , bootstrap = require('bootstrap-stylus');
 
 var app = express();
+
+//function used to configure bootstrap-stylus
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(bootstrap());
+}
+
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -20,6 +30,10 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
   app.use(require('connect-assets')());
+  app.use(stylus.middleware({
+		src: __dirname + '/public',
+		compile: compile
+	}));
 });
 
 app.configure('development', function(){
