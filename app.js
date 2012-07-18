d662d64f92af34c5ecaa9938b246e3ca2cdbf7a2
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-07-18 12:46:14 +0300

    basic authentication using Passport

--- app.js
+++ app.js
@@ -8,9 +8,79 @@ var express = require('express')
   , http = require('http')
   , stylus = require('stylus')
   , bootstrap = require('bootstrap-stylus')
-  , mongoose = require('mongoose');
+  , mongoose = require('mongoose')
+  , passport = require('passport')  
+  , flash = require('connect-flash')
+  , LocalStrategy = require('passport-local').Strategy;
 
 mongoose.connect('mongodb://localhost/microcommunity');
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+
+
+// Use the LocalStrategy within Passport.
+//   Strategies in passport require a `verify` function, which accept
+//   credentials (in this case, a username and password), and invoke a callback
+//   with a user object.  In the real world, this would query a database;
+//   however, in this example we are using a baked-in set of users.
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+
 
 var Comments = new mongoose.Schema({
     name     : String
@@ -63,6 +133,11 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser('keyboard cat'));
+  app.use(flash());
+  app.use(express.session());  
+  app.use(passport.initialize());
+  app.use(passport.session());  
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
   app.use(require('connect-assets')());
@@ -76,13 +151,36 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+
+
+app.get('/account', ensureAuthenticated, function(req, res){
+  res.render('account', { user: req.user });
+});
+
+
+app.get('/login', function(req, res){
+  res.render('login', { user: req.user, message: req.flash('error') });
+});
+
+app.post('/login', 
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res) {
+    res.redirect('/');
+});
+  
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+  
+
 //app.get('/', routes.index);
 
 app.get('/', function(req, res){
   Post.find(function(err, posts) {
 	  Wikipage.find(function(err, wikipages) {
 			console.log(posts);
-			res.render('index', { title: 'MicroCommunity', posts : posts, wikipages: wikipages });
+			res.render('index', { title: 'MicroCommunity', posts : posts, wikipages: wikipages, user: req.user });
 	  });
   });
 });
@@ -205,4 +303,16 @@ app.post('/api/wikipages', function(req, res){
 
 
 app.listen(3000);
+
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
 
