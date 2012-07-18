2d12a8df1b52da0651f2cf836e290c88f101c27a
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-07-18 20:46:08 +0300

    connecting the Passport authentication to db

--- app.js
+++ app.js
@@ -15,29 +15,10 @@ var express = require('express')
 
 mongoose.connect('mongodb://localhost/microcommunity');
 
-var users = [
-    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
-  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
-];
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
+var User = mongoose.model('User', new mongoose.Schema({
+	email: String,
+	password: String
+}));
 
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
@@ -45,12 +26,12 @@ function findByUsername(username, fn) {
 //   this will be as simple as storing the user ID when serializing, and finding
 //   the user by ID when deserializing.
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  done(null, user._id);
 });
 
 passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
-    done(err, user);
+	User.findById(id, function(err, user) {
+		done(err, user);
   });
 });
 
@@ -62,7 +43,7 @@ passport.deserializeUser(function(id, done) {
 //   with a user object.  In the real world, this would query a database;
 //   however, in this example we are using a baked-in set of users.
 passport.use(new LocalStrategy(
-  function(username, password, done) {
+  function(email, password, done) {
     // asynchronous verification, for effect...
     process.nextTick(function () {
       
@@ -70,12 +51,18 @@ passport.use(new LocalStrategy(
       // username, or the password is not correct, set the user to `false` to
       // indicate failure and set a flash message.  Otherwise, return the
       // authenticated `user`.
-      findByUsername(username, function(err, user) {
-        if (err) { return done(err); }
-        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
-        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
-        return done(null, user);
-      })
+      
+  		
+
+      
+   	User.findOne({ email: email }, function(err, user){
+   			console.log(user);
+		    if (err) { return done(err); }
+		    if (!user) { return done(null, false, { message: 'Unknown email ' + email }); }
+		    if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+		    return done(null, user);
+    	});
+    	
     });
   }
 ));
@@ -86,6 +73,7 @@ var Comments = new mongoose.Schema({
     name     : String
   , text      : String
 });
+
 
 var Post = mongoose.model('Post', new mongoose.Schema({
   name: String,
@@ -167,11 +155,21 @@ app.get('/signup', function(req, res){
   res.render('signup', { user: req.user });
 });
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
-  function(req, res) {
-    res.redirect('/');
+app.post('/login', function(req, res, next) {
+  passport.authenticate('local', function(err, user, info) {
+
+    if (err) { return next(err) }
+    if (!user) {
+      req.flash('error', info.message);
+      return res.redirect('/login')
+    }
+    req.logIn(user, function(err) {
+      if (err) { return next(err); }
+      return res.redirect('/');
+    });
+  })(req, res, next);
 });
+
   
 app.get('/logout', function(req, res){
   req.logout();
@@ -318,6 +316,6 @@ app.listen(3000);
 //   login page.
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
+  res.redirect('/login');
 }
 
