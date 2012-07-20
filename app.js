1f097704ce93ec270ec0b6bc2402dbef8bfa7fc0
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-07-20 16:25:18 +0300

    using separating the posts api into a separate module using express-resource

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ var express = require('express')
   , flash = require('connect-flash')
   , _ = require('underscore')
   , LocalStrategy = require('passport-local').Strategy
-  , async = require('async');
+  , async = require('async')
+  , Resource = require('express-resource'); 
+
 
 mongoose.connect('mongodb://localhost/microcommunity');
 
@@ -123,6 +125,7 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+app.resource('api/posts', require('./api/posts'));  
 
 
 app.get('/account', ensureAuthenticated, function(req, res){
@@ -266,184 +269,6 @@ Post.find(function(err, posts) {
 
   });		
 	  });
-});
-
-
-app.get('/api/posts', function(req, res){
-  return Post.find(function(err, posts) {
-  	
-  	if(!err){
-  	
-  		var userIds = _.pluck(posts, "user");
-  		
-  		User.find({ _id : { $in : userIds } }, function(err, users){
-  			var map = {};
-  			_.each(users, function(u){
-  				map[u.id] = u;
-  			});
-  			
-  			var joinedposts = [];
-  			var joinedpost = {};
- 				var functions = []; 			
-  			
-  			_.each(posts, function(p){  			
-  			
-  				function myfunction(callback) {
-						var user = {
-							_id : map[p.user]._id,
-							email: map[p.user].email  				  				
-						};  				
-						
-						var functions = [];    		
-		  			var final_comments = [];
-		  			var j = 0;
-		  	
-						for(var i=0; i< p.comments.length; i++){
-
-							function myfunction(callback){							
-								comment = p.comments[j];
-								j++;
-							
-								User.findById(comment.user, function(err, user){
-									var joinedcomment = {
-										text : comment.text,
-										name : comment.name,
-										_id  : comment._id,
-										user : user,
-										created_at : comment.created_at
-									};
-								
-									final_comments.push(joinedcomment);		
-								
-									callback(null);
-								
-								});    			
-							}    			
-							functions.push(myfunction);   		    			
-						}
-		  		
-						async.waterfall(functions, function(err, result){
-							var joinedpost = {
-								_id  : p._id,
-								name : p.name,
-								text : p.text,
-								user : user,
-								created_at : p.created_at,
-								comments: final_comments
-							};
-					  	joinedposts.push(joinedpost);     			
-					  	callback(null);
-						});
-						
-		  		  				
-  				}
-  				
-  				functions.push(myfunction);  						
-  			});
-  			
-				async.waterfall(functions, function(err, result){
-					return res.send(joinedposts);					
-				});	
-  			
-  			
-  		});
-
-  	}
-
-  });
-});
-
-app.get('/api/posts/:id', function(req, res){
-  return Post.findById(req.params.id, function(err, post) {
-    if (!err) {
-    	User.findById(post.user, function(err, user){
-    	
-    		var functions = [];    		
-    		var final_comments = [];
-    		var j = 0;
-    	
-    		for(var i=0; i< post.comments.length; i++){
-    			var comment = post.comments[i];
-    			
-    			function myfunction(callback){
-    			
-    				comment = post.comments[j];
-    				j++;
-		  			
-		  			User.findById(comment.user, function(err, user){
-		  				var joinedcomment = {
-		  					text : comment.text,
-		  					name : comment.name,
-		  					_id  : comment._id,
-		  					user : user,
-		  					created_at: comment.created_at
-		  				};
-		  				
-		  				final_comments.push(joinedcomment);		
-		  				
-		  				callback(null);
-		  				
-		  			});    			
-    			}
-    			functions.push(myfunction);   		    			
-    		}
-    		
-    		async.waterfall(functions, function(err, result){
-	    		var joinedpost = {
-		  			_id  : post._id,
-		  			name : post.name,
-		  			text : post.text,
-		  			user : user,
-		  			created_at : post.created_at,
-		  			comments: final_comments
-    			};
-	      	return res.send(joinedpost);     			
-    		});    		
-   	
-    	});
-    }
-  });
-});
-
-app.put('/api/posts/:id', function(req, res){
-  return Post.findById(req.params.id, function(err, post) {
-    post.text = req.body.text;
-    post.name = req.body.name;
-    return post.save(function(err) {
-      if (!err) {
-        console.log("updated");
-      }
-      return res.send(post);
-    });
-  });
-});
-
-app.post('/api/posts', function(req, res){
-
-  var post;
-  post = new Post({
-    text: req.body.text,
-    name: req.body.name,
-    user: mongoose.Types.ObjectId(req.body.user._id),
-    created_at : req.body.created_at
-  });
-  
-  console.log(post	);
-  post.save(function(err) {
-    if (!err) {
-      return console.log("created");
-    }
-  });
-  
-	var joinedpost = {
-		_id  : post._id,
-		name : post.name,
-		text : post.text,
-		user : req.body.user,
-		created_at : post.created_at
-	};
-  return res.send(joinedpost);    	  
- 
 });
 
 
