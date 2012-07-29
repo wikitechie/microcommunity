f4cad19704ccfa31ca901e29afe82393d3b1e12e
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2012-07-29 06:43:49 +0300

    minor fixes to client code build.js

--- public/js/build.js
+++ public/js/build.js
@@ -2,7 +2,7 @@
 
 	appDir : "../js",
 	baseUrl: '.',
-	dir : '../final',
+	dir : '../compiled',
 	
   paths: {
     cs: 'cs',
@@ -10,8 +10,17 @@
     jquery : 'lib/jquery.min',
     backbone : 'lib/backbone.min',
     underscore: 'lib/underscore.min',
-    bootstrap: 'lib/bootstrap.min'
+    bootstrap: 'lib/bootstrap.min',
+    'jquery.gravatar': 'lib/jquery.gravatar',
+    'jquery.spin': 'lib/jquery.spin',
+    'spin': 'lib/spin.min',
+    'md5' : 'lib/md5',
+    'general' : 'lib/general',
+    'moment' : 'lib/moment.min',
+    'diff' : 'lib/diff'
   },
+  
+  mainConfigFile: 'main.js',
   
   optimize: "none",  
   
