6486c3248a524970237bc241517d2f2ed458f3c2
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2013-03-28 14:02:28 +0200

    wall reverseRelation for Item models

--- client/modelsdraft/core.js
+++ client/modelsdraft/core.js
@@ -6,28 +6,30 @@ define([
 	User = Backbone.RelationalModel.extend({
 	})
 
-	Item = Backbone.RelationalModel.extend({
+	Item = Backbone.RelationalModel.extend({	
+		constructor : function(){
+		
+			/* a small  hack in order to prevent the model from 
+				 overriding the 'wall' reverseRelation which 
+				 is set in the wall model */
+				 
+			this.relations.push({
+				type : Backbone.HasOne,
+				key : 'author',
+				relatedModel : 'User',
+				includeInJSON : Backbone.Model.prototype.idAttribute	
+			})
+			
+			Backbone.RelationalModel.prototype.constructor.apply(this, arguments)
+		},
+		
 		subModelTypeAttribute : 'itemType',
 		subModelTypes : {
 			'post' : 'Post'
 		},
 		defaults : {
 			objectType : 'item'
-		},
-		relations : [
-			{
-				type : Backbone.HasOne,
-				key : 'author',
-				relatedModel : 'User',
-				includeInJSON : Backbone.Model.prototype.idAttribute
-			},
-			{
-				type : Backbone.HasOne,
-				key : 'wall',
-				relatedModel : 'Wall',
-				includeInJSON : Backbone.Model.prototype.idAttribute
-			}				
-		]						
+		}				
 	})
 
 	Items = Backbone.Collection.extend({
@@ -49,9 +51,10 @@ define([
 				+ this.get('content')		
 		}	
 	})
+	
 	//building an instance in order to setup relations	
 	var post = new Post()
-	Backbone.Relational.store.reset()							
+	Backbone.Relational.store.reset()						
 
 	Wall = Backbone.RelationalModel.extend({
 		relations : [
@@ -60,7 +63,12 @@ define([
 				key : 'items',
 				relatedModel : 'Item',
 				collectionType : 'Items',
-				includeInJSON : Backbone.Model.prototype.idAttribute,
+				includeInJSON : Backbone.Model.prototype.idAttribute,				
+				reverseRelation : {
+					key : 'wall',
+					type : Backbone.HasOne,					
+					includeInJSON : Backbone.Model.prototype.idAttribute,														
+				}			
 			},
 			{
 				type : Backbone.HasOne,
--- test/client/newclient/core.js
+++ test/client/newclient/core.js
@@ -94,7 +94,6 @@ define([
 							content : "Hello, World!",
 							author : user,
 							itemType : 'post',
-							wall : 'wall-1'
 						}] 
 					})							
 				})		
@@ -112,8 +111,10 @@ define([
 				
 				it ('should insert an item associated with the wall', function(){
 					console.log(wall.get('items').first().toJSON())
-					assert.equal(wall.get('items').first().get('wall') instanceof Wall, true)					
-				})								
+					assert.equal(wall.get('items').first().get('wall') instanceof Wall, true)
+					
+				})
+								
 			})
 		})
 	
@@ -132,6 +133,59 @@ define([
 				wall.get('owner').id.should.equal( user.id)
 			})			
 		})					
-	})											
+	})
+	
+	describe('Post model', function(){			
+		describe('A post on author\'s own wall', function(){
+			before(function(){
+				this.user = new User({name : 'Name', id : 'user-1' , wall : 'wall-1' })	
+				this.wall = new Wall({id : 'wall-1', owner : 'user-1'	})	
+				this.wall.get('items').add({
+					content : "Hello, World!",
+					author : this.user,
+					itemType : 'post'							
+				}, { at : 0 })										
+			})
+			after(function(){
+				this.user = null
+				this.wall = null
+				Backbone.Relational.store.reset()
+			}) 
+			it ('should be associated back to its wall', function(){
+				assert.ok(this.wall.get('items').first().get('wall'))
+				assert.ok(this.wall.get('items').first().get('wall').id)				
+				assert.equal(this.wall.get('items').first().get('wall').id, this.wall.id)
+			})			
+			it ('should display the right message', function(){
+				assert.equal(this.wall.get('items').first().msg(), "Name posted on his wall Hello, World!")				
+			})			
+		})
+		
+		describe('A post on another user\'s wall', function(){
+			before(function(){
+				this.user1 = new User({name : 'User 1', id : 'user-1' , wall : 'wall-1' })
+				this.user2 = new User({name : 'User 2', id : 'user-2' , wall : 'wall-2' })							
+				this.wall1 = new Wall({id : 'wall-1', owner : 'user-1'	})	
+				this.wall2 = new Wall({id : 'wall-2', owner : 'user-2'	})											
+				this.wall2.get('items').add({
+					content : "Hello, User 2! I am User 1",
+					author : this.user1,
+					itemType : 'post'							
+				}, { at : 0 })										
+			})				
+			after(function(){
+				this.user1 = null
+				this.user2 = null
+				this.wall1 = null
+				this.wall2 = null
+				Backbone.Relational.store.reset()
+			})
+			it ('should display the right message', function(){
+				this.wall2.get('items').first().msg()
+					.should.equal("User 1 posted on User 2's wall Hello, User 2! I am User 1")
+			})			
+		})
+	})		
+												
 						
 })
