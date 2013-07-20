b9d861efcf5b7ec1bae5bac6c8af81d7dcfe5583
al-Amjad Tawfiq Isstaif <isstaif@gmail.com>
2013-07-20 06:48:45 +0300

    supporting comments on photos

--- mc_modules/microcommunity-basic/client/models/items/photo.js
+++ mc_modules/microcommunity-basic/client/models/items/photo.js
@@ -2,12 +2,14 @@ define([
 	'bb',
 	'models/item',	
 	'views/items/photo',
+	'views/item-plugins/comments',	
 	'text!templates/items/post/message.html'	
-], function(Backbone, Item, PhotoView, messageTemplate){
+], function(Backbone, Item, PhotoView, Comments, messageTemplate){
 	var Photo = Item.extend({	
 		collection : 'photos',		
 		contentView : PhotoView,
-		messageTemplate : messageTemplate
+		messageTemplate : messageTemplate,
+		pluginView : Comments		
 	})	
 	return Photo
 })
--- mc_modules/microcommunity-basic/models/photo.js
+++ mc_modules/microcommunity-basic/models/photo.js
@@ -3,10 +3,29 @@ var mongoose = require('mongoose')
 	, items = require('microcommunity').items
 	, isItem = models.plugins.isItem
 
+
+var commentSchema = new mongoose.Schema({
+	content : String,
+	author : { type : mongoose.Schema.Types.ObjectId, ref : 'User' },
+	published : Date,
+	can : {}
+})
+
+commentSchema.pre('save', function(next){
+	this.published = new Date()
+	next()
+})
+
 var photoSchema = new mongoose.Schema({
 	content: String,
 	filePath : String,
+	comments : [ commentSchema ]	
 })
+
+photoSchema.statics.populateItem = function(doc, next){
+	var User = mongoose.model('User')
+	User.populate(doc.comments, 'author', next)
+}
 
 photoSchema.plugin(isItem, { objectType : 'photo' })
 
